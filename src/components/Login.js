daf3f52fb7aab212db4851545a0818dcb509557c
estebanpena <esteban.pena.2019@gmail.comr>
2021-10-06 11:26:02 -0400

    history to hashHistory

--- src/components/Login.js
+++ src/components/Login.js
@@ -28,7 +28,7 @@ class Login extends Component {
                 password:this.state.password
             })
         })
-        this.props.history.push('/');
+        this.props.hashHistory.push('/');
     } 
 
     render() {
