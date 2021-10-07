b11a5501236566c85d0a96f4d97e4c28fa011839
estebanpena <esteban.pena.2019@gmail.comr>
2021-10-07 19:36:19 -0400

    fixed navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,40 @@
 import React, { Component } from 'react';
-import * as ReactBootstrap from 'react-bootstrap';
+//import * as ReactBootstrap from 'react-bootstrap';
+import Navbar from "react-bootstrap/Navbar";
+import { Nav } from "react-bootstrap";
+import { NavDropdown } from "react-bootstrap";
 
 
 class Navbar extends Component {
     render() {
         return(
             <div className='primary--nav'>
-              <ReactBootstrap.Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <ReactBootstrap.Container>
-                <ReactBootstrap.Navbar.Brand href="/First-Website-Using-React">.informat*ve</ReactBootstrap.Navbar.Brand>
-                <ReactBootstrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <ReactBootstrap.Navbar.Collapse id="responsive-navbar-nav">
-                  <ReactBootstrap.Nav className="me-auto">
-                    <ReactBootstrap.Nav.Link href="/First-Website-Using-React/login">Log In</ReactBootstrap.Nav.Link>
-                    <ReactBootstrap.Nav.Link href="/First-Website-Using-React/register">Sign Up</ReactBootstrap.Nav.Link>
-                    <ReactBootstrap.NavDropdown title="More" id="collasible-nav-dropdown">
-                      <ReactBootstrap.NavDropdown.Item href="#action/3.1">Action</ReactBootstrap.NavDropdown.Item>
-                      <ReactBootstrap.NavDropdown.Item href="#action/3.2">Another action</ReactBootstrap.NavDropdown.Item>
-                      <ReactBootstrap.NavDropdown.Item href="#action/3.3">Something</ReactBootstrap.NavDropdown.Item>
-                      <ReactBootstrap.NavDropdown.Divider />
-                      <ReactBootstrap.NavDropdown.Item href="#action/3.4">Separated link</ReactBootstrap.NavDropdown.Item>
-                    </ReactBootstrap.NavDropdown>
-                  </ReactBootstrap.Nav>
-                  <ReactBootstrap.Nav>
-                    <ReactBootstrap.Nav.Link href="/First-Website-Using-React/login">Log In</ReactBootstrap.Nav.Link>
-                    <ReactBootstrap.Nav.Link  
+              <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                <Navbar.Brand href="/First-Website-Using-React">.informat*ve</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                  <Nav className="me-auto">
+                    <Nav.Link href="/First-Website-Using-React/login">Log In</Nav.Link>
+                    <Nav.Link href="/First-Website-Using-React/register">Sign Up</Nav.Link>
+                    <NavDropdown title="More" id="collasible-nav-dropdown">
+                      <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                      <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                      <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                      <NavDropdown.Divider />
+                      <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                    </NavDropdown>
+                  </Nav>
+                  <Nav>
+                    <Nav.Link href="/First-Website-Using-React/login">Log In</Nav.Link>
+                    <Nav.Link  
                       href="/First-Website-Using-React/register">
                       Sign Up
-                    </ReactBootstrap.Nav.Link>
-                    </ReactBootstrap.Nav>
-                  </ReactBootstrap.Navbar.Collapse>
-                </ReactBootstrap.Container>
-              </ReactBootstrap.Navbar>
+                    </Nav.Link>
+                    </Nav>
+                  </Navbar.Collapse>
+                </Container>
+              </Navbar>
             </div>
         );
     }
