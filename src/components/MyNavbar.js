61c3c13bb583597f380f07b4d9a7ff5b0e47cfa5
estebanpena <esteban.pena.2019@gmail.comr>
2021-10-07 20:42:59 -0400

    tweaks

--- src/components/MyNavbar.js
+++ src/components/MyNavbar.js
@@ -11,32 +11,30 @@ class MyNavbar extends Component {
         return(
             <div className='primary--nav'>
                 <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                    <Container>
-                        <Navbar.Brand href="/First-Website-Using-React">.informat*ve</Navbar.Brand>
-                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                        <Navbar.Collapse id="responsive-navbar-nav">
-                            <Nav className="me-auto">
-                                <Nav.Link href="/First-Website-Using-React/login">Log In</Nav.Link>
-                                <Nav.Link href="/First-Website-Using-React/register">Sign Up</Nav.Link>
-                                <NavDropdown title="More" id="collasible-nav-dropdown">
-                                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                    <NavDropdown.Divider />
-                                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                                </NavDropdown>
-                              </Nav>
+                    <Navbar.Brand href="/First-Website-Using-React">.informat*ve</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="/First-Website-Using-React/login">Log In</Nav.Link>
+                            <Nav.Link href="/First-Website-Using-React/register">Sign Up</Nav.Link>
+                            <NavDropdown title="More" id="collasible-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
 
-                              <Nav>
-                                  <Nav.Link href="/First-Website-Using-React/login">Log In</Nav.Link>
-                                  <Nav.Link  
-                                      href="/First-Website-Using-React/register">
-                                      Sign Up
-                                  </Nav.Link>
-                                </Nav>
-                          </Navbar.Collapse>
-                    </Container>
-              </Navbar>
+                        <Nav>
+                            <Nav.Link href="/First-Website-Using-React/login">Log In</Nav.Link>
+                            <Nav.Link  
+                                href="/First-Website-Using-React/register">
+                                Sign Up
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
             </div>
         );
     }
