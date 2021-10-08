24453b68ef53d5cb9eb993f86aa648b2460c2811
estebanpena <esteban.pena.2019@gmail.comr>
2021-10-07 20:09:04 -0400

    added linkcontainer

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap'
 import Routes from './routes';
 import MyNavbar from './components/MyNavbar';
 
 const App = () => {
   return (
     <Router basename='/React'>
-        <MyNavbar />
-        <Routes />  
+        <Routes /> 
+        <LinkContainer>
+            <MyNavbar />
+        </LinkContainer>
     </Router>
   ); 
 }
